3e0f190c4c38e794192898bee4ad71a7fe24bfc5
Denis Boyev <susanindin@gmail.com>
2017-06-16 16:04:09 +0600

    60 complete

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Quote} from "../../data/quote.interface";
 import {QuotesService} from "../../services/quotes";
+import {ModalController} from "ionic-angular";
+import {QuotePage} from "../quote/quote";
 
 @Component({
   selector: 'page-favorites',
@@ -8,11 +10,17 @@ import {QuotesService} from "../../services/quotes";
 })
 export class FavoritesPage {
   qoutes: Quote[];
-  constructor (private qouteService: QuotesService) {
+  constructor (private qouteService: QuotesService,
+               private modalCtrl: ModalController) {
 
   }
 
   ionViewWillEnter() {
     this.qoutes = this.qouteService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote){
+    const modal = this.modalCtrl.create(QuotePage, quote);
+    modal.present();
   }
 }
